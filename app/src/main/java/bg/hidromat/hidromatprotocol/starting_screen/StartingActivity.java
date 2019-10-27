aa561de75fa73f7ffbb4576f2e18cf8e7fd6422f
milen <m.dimitrov@futuresoft.bg>
2019-10-27 22:52:24 +0200

    Initial commit

---
+++ app/src/main/java/bg/hidromat/hidromatprotocol/starting_screen/StartingActivity.java
@@ -0,0 +1,16 @@
+package bg.hidromat.hidromatprotocol.starting_screen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import bg.hidromat.hidromatprotocol.R;
+
+public class StartingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting);
+    }
+}
